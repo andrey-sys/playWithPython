1d5f7eb664b8b4813463c6c8de8a72ce33b69a79
Andrey Yershov <andreyyersov424@gmail.com>
2021-01-29 18:30:02 +0200

    edit readme file

--- venv/Lesson_2.py
+++ venv/Lesson_2.py
@@ -4,13 +4,14 @@ print(expression+ str(answer))
 
 print('  Creativaty, Imegination, Strategy  '*3)#5 dog
 
-# phrase=input()
-# print(phrase)
-# print('      Benjamin Disraeli')
+print('Enter any frase of Benjamin Disraeli: ')
+phrase=input()
+print(phrase)
+print('      Benjamin Disraeli')
 
-print('enter number from 0 to 10')
+print('enter number from 0 to 10, to multiply it')
 a=int(input())
-print('enter again number from 0 to 10')
+print('enter again number from 0 to 10, to multiply it')
 b=int(input())
 print('your answer')
 print(a*b)
@@ -21,4 +22,17 @@ word = 'Bye'
 phrase = word * 3 + '!'
 print(phrase)
 
+print('Enter any number for getting remainder')
+n = int(input())
+print(n % 256)
 
+print('Enter any number not lower than 100: you will get the root of the number')
+number=int(input())
+print(2**number)
+
+n = 111
+a=n//100
+k=n%100
+b=k//10
+c=k%10
+print(a+b+c)
--- venv/Lesson_3.py
+++ venv/Lesson_3.py
@@ -4,4 +4,18 @@ N = int(input())
 print('enter number of apples: ')
 K = int(input())
 print('how many apples get any of the people?')
-print('answer is: ',K//N,' apples will get each of the pupils')
+print('answer is: ', K // N, ' apples will get each of the pupils')
+
+print('exercise 2')
+print('enter dollar price: ')
+a = int(input())
+print('enter cent price: ')
+b = int(input())
+print('enter dollar price: ')
+c = int(input())
+print('enter cent price: ')
+d = int(input())
+cost1 = a * 100 + b
+cost2 = c * 100 + d
+print('the total cost of two parts is: ', (cost1 + cost2) // 100,'.' ,(cost1 + cost2) % 100)
+
