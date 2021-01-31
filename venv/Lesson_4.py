a878186c3c570f6394e48f5e0bb4c04ffc2eb57d
Andrey Yershov <andreyyersov424@gmail.com>
2021-01-31 22:03:01 +0200

    add new class add new exercises refactor some code

--- venv/Lesson_4.py
+++ venv/Lesson_4.py
@@ -1,13 +1,32 @@
-#exercise_1
-#if number is even or not
+#conditions <= >= < > ==
+
+# exercise_1
+# if number is even or not
 print('Enter any number')
 number = int(input())
 isEven = number % 2 == 0
 print(isEven)
 
-#exercise_2
-#if two events have crosspoint
-print('Enter beginning of the first event')
-startEvent=int(input())
-print('Enter ending of the first event')
-finishEvent=int(input())
+# exercise_2
+# if two lines have a crosspoint
+print('Enter beginning of the first line')
+startFirstLine = int(input())
+print('Enter ending of the first line')
+finishFirstLine = int(input())
+print('Enter beginning of the second line')
+startSecondLine = int(input())
+print('Enter ending of the second line')
+finishSecondLine = int(input())
+ifStartFirstLineInSecond = startSecondLine<=startFirstLine<= finishSecondLine
+ifFinishFirstLineInSecond = startSecondLine<=finishFirstLine<=finishSecondLine
+ifStartSecondLineInFirst = startFirstLine<=startSecondLine<=finishFirstLine
+ifFinishSecondLineInFirst = startFirstLine <= finishSecondLine<= finishFirstLine
+theAnswer=ifStartFirstLineInSecond or ifFinishFirstLineInSecond or\
+          ifStartSecondLineInFirst or ifFinishSecondLineInFirst
+print('The answer is : ',theAnswer,  '  ->if you get "true" you have the crossline, '
+                                    'if you get "false" you have not the crossline' )
+
+#or simplify it in one row
+theAnswerSimplified = startFirstLine<=finishSecondLine and startSecondLine <= finishFirstLine
+print('The answer is : ',theAnswerSimplified,  '  ->if you get "true" you have the crossline, '
+                                    'if you get "false" you have not the crossline' )
---
+++ venv/Lesson_5.py
@@ -0,0 +1,10 @@
+# if statement
+
+# exercise_1
+# set any number and turn it to positive number
+print('Enter any number: ')
+number = int(input())
+if number >= 0:
+    print(number)
+else:
+    print(-number)
