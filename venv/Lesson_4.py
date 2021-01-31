c95ce3a60746cafae07581cec3af18fc3555301c
Andrey Yershov <andreyyersov424@gmail.com>
2021-01-31 20:03:35 +0200

    add new class add new exercises refactor some code

--- venv/Lesson_4.py
+++ venv/Lesson_4.py
@@ -6,4 +6,8 @@ isEven = number % 2 == 0
 print(isEven)
 
 #exercise_2
-#if number is even or not
+#if two events have crosspoint
+print('Enter beginning of the first event')
+startEvent=int(input())
+print('Enter ending of the first event')
+finishEvent=int(input())
