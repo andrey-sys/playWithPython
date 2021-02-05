e6eefc2c9b8bfca5b4b841dfd90f52cd95064bb1
Andrey Yershov <andreyyersov424@gmail.com>
2021-02-05 17:51:51 +0200

    add new exercises

--- venv/Lesson_5.py
+++ venv/Lesson_5.py
@@ -1,13 +1,13 @@
-# # if statement
-#
-# # exercise_1
-# # set any number and turn it to positive number
-# print('Enter any number: ')
-# number = int(input())
-# if number >= 0:
-#     print(number)
-# else:
-#     print(-number)
+# if statement
+
+# exercise_1
+# set any number and turn it to positive number
+print('Enter any number: ')
+number = int(input())
+if number >= 0:
+    print(number)
+else:
+    print(-number)
 
 # exercise_2
 # find the biggest number between two numbers
@@ -20,3 +20,19 @@ if a>=b:
 else:
     print(b)
 
+# exercise_3
+# find the quarter of coordinates
+print('Enter X coordinate')
+x = int(input())
+print('Enter Y coordinate')
+y = int(input())
+if x > 0:
+    if y > 0:
+        print('There is first quarter')
+    else:
+        print('There is forth quarter')
+else:
+    if y > 0:
+        print('There is second quarter')
+    else:
+        print('There is third quarter')
