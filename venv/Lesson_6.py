879c8a8087505eb7fd2fffc556de1d0bbd69d145
Andrey Yershov <andreyyersov424@gmail.com>
2021-02-05 18:29:28 +0200

    add new exercise

--- venv/Lesson_6.py
+++ venv/Lesson_6.py
@@ -1,3 +1,11 @@
 # while statement
 
 # exercise_1
+# Complete up to a hundred
+print('Enter number not bigger than 100: ')
+number = int(input())
+while number<=100:
+    print(number, end=' ')
+    number=number+1
+print('The end')
+
