3549eaf3a2fefe815022ebf42b9360e50612d7b9
Andrey Yershov <andreyyersov424@gmail.com>
2021-02-05 17:23:03 +0200

    add new exercises

--- venv/Lesson_5.py
+++ venv/Lesson_5.py
@@ -1,10 +1,22 @@
-# if statement
+# # if statement
+#
+# # exercise_1
+# # set any number and turn it to positive number
+# print('Enter any number: ')
+# number = int(input())
+# if number >= 0:
+#     print(number)
+# else:
+#     print(-number)
 
-# exercise_1
-# set any number and turn it to positive number
-print('Enter any number: ')
-number = int(input())
-if number >= 0:
-    print(number)
+# exercise_2
+# find the biggest number between two numbers
+print('Enter first number')
+a = int(input())
+print('Enter second number')
+b = int(input())
+if a>=b:
+    print(a)
 else:
-    print(-number)
+    print(b)
+
